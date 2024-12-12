5f5a44fb657b0fb3cfd215e1ae2fb407344fc2b3
Wael <wael@Waels-Mac-mini.local>
2024-12-12 22:35:56 +0200

    enhance app

--- app/(dashboard)/checkout/page.js
+++ app/(dashboard)/checkout/page.js
@@ -1,5 +1,8 @@
 import CheckoutPage from "../../_components/CheckoutPage";
 
 export default function page(){
+    if (typeof window === "undefined") {
+        return null; // Avoid rendering on the server
+    }
     return <CheckoutPage />
 }
