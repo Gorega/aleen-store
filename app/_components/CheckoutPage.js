05d36b2f3ff02275def8441e75b949d02eac63e7
Wael <wael@Waels-Mac-mini.local>
2024-12-12 23:44:33 +0200

    a

--- app/_components/CheckoutPage.js
+++ app/_components/CheckoutPage.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, useLayoutEffect, useState } from "react";
+import { useState } from "react";
 import styles from "../_styles/Checkout.module.css";
 import Image from "next/image";
 import { ContextApi } from "../_util/GlobalContext";
@@ -8,11 +8,11 @@ import { ContextApi } from "../_util/GlobalContext";
 
 export default function CheckoutPage(){
 
-    const [products,setProducts] = useState([]);
+    const [products] = useState([]);
     const [discountCode,setDiscountCode] = useState("");
     const [formDataStatus] = useState({});
     const [formErrors] = useState({});
-    const {getLocalStorageItems} = useContext(ContextApi);
+    // const {getLocalStorageItems} = useContext(ContextApi);
 
     const deliveryPlaces = [{
         id:"west_bank",
@@ -89,21 +89,21 @@ export default function CheckoutPage(){
     }
 
 
-    useLayoutEffect(()=>{
-        if (typeof window !== "undefined") {
-            getLocalStorageItems("cart", setProducts);
-        }
-    },[])
+    // useLayoutEffect(()=>{
+    //     if (typeof window !== "undefined") {
+    //         getLocalStorageItems("cart", setProducts);
+    //     }
+    // },[])
 
-    if(!products || products.length === 0){
-        return <div className={`${styles.empty} container`}>
-            <div className={styles.content}>
-                <span><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><g id="Shopping_Cart"><path d="M17.437,19.934c0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c0,-0.552 0.448,-1 1,-1c0.552,0 1,0.448 1,1Zm-11.217,-4.266l-0.945,-10.9c-0.03,-0.391 -0.356,-0.693 -0.749,-0.693l-0.966,-0c-0.276,-0 -0.5,-0.224 -0.5,-0.5c0,-0.276 0.224,-0.5 0.5,-0.5l0.966,-0c0.916,-0 1.676,0.704 1.746,1.617l0.139,1.818l13.03,-0c0.885,-0 1.577,0.76 1.494,1.638l-0.668,7.52c-0.121,1.285 -1.199,2.267 -2.489,2.267l-9.069,0c-1.29,0 -2.367,-0.981 -2.489,-2.267Zm0.274,-8.158l0.722,8.066c0.073,0.77 0.719,1.359 1.493,1.359l9.069,0c0.774,0 1.42,-0.589 1.493,-1.359l0.668,-7.518c0.028,-0.294 -0.203,-0.548 -0.498,-0.548l-12.947,-0Zm4.454,12.424c-0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c-0,-0.552 0.448,-1 1,-1c0.552,0 1,0.448 1,1Z"></path></g></svg></span>
-                <h2>عربة التسوق فارغة</h2>
-                <p>ليس لديك أي منتجات في عربة التسوق حتى الان للقيام بطلبها.</p>
-            </div>
-        </div>
-    }
+    // if(!products || products.length === 0){
+    //     return <div className={`${styles.empty} container`}>
+    //         <div className={styles.content}>
+    //             <span><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><g id="Shopping_Cart"><path d="M17.437,19.934c0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c0,-0.552 0.448,-1 1,-1c0.552,0 1,0.448 1,1Zm-11.217,-4.266l-0.945,-10.9c-0.03,-0.391 -0.356,-0.693 -0.749,-0.693l-0.966,-0c-0.276,-0 -0.5,-0.224 -0.5,-0.5c0,-0.276 0.224,-0.5 0.5,-0.5l0.966,-0c0.916,-0 1.676,0.704 1.746,1.617l0.139,1.818l13.03,-0c0.885,-0 1.577,0.76 1.494,1.638l-0.668,7.52c-0.121,1.285 -1.199,2.267 -2.489,2.267l-9.069,0c-1.29,0 -2.367,-0.981 -2.489,-2.267Zm0.274,-8.158l0.722,8.066c0.073,0.77 0.719,1.359 1.493,1.359l9.069,0c0.774,0 1.42,-0.589 1.493,-1.359l0.668,-7.518c0.028,-0.294 -0.203,-0.548 -0.498,-0.548l-12.947,-0Zm4.454,12.424c-0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c-0,-0.552 0.448,-1 1,-1c0.552,0 1,0.448 1,1Z"></path></g></svg></span>
+    //             <h2>عربة التسوق فارغة</h2>
+    //             <p>ليس لديك أي منتجات في عربة التسوق حتى الان للقيام بطلبها.</p>
+    //         </div>
+    //     </div>
+    // }
 
 
     return <div className={`${styles.checkout} container`}>
