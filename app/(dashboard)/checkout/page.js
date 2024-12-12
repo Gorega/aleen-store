9950746290e980846115abcebf451809d41d1899
Wael <wael@Waels-Mac-mini.local>
2024-12-12 22:43:14 +0200

    enhance app

--- app/(dashboard)/checkout/page.js
+++ app/(dashboard)/checkout/page.js
@@ -1,8 +1,5 @@
 import CheckoutPage from "../../_components/CheckoutPage";
 
 export default function page(){
-    if (typeof window === "undefined") {
-        return null; // Avoid rendering on the server
-    }
     return <CheckoutPage />
 }
