ea1c9e6cd12b9f08b0650b027ebb0139d1e23b9d
Wael <wael@Waels-Mac-mini.local>
2024-12-14 21:35:00 +0200

    enhance app

--- mongoose/url.js
+++ mongoose/url.js
@@ -1,1 +1,1 @@
-export const url = process.env.NODE_ENV === "development" ? "http://localhost:3000/" : process.env.NEXT_AUTH_URL
+export const url = process.env.NODE_ENV === "development" ? "http://localhost:3000/" : "https://aleen-store.vercel.app/"
