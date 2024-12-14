f71a12d0503513fe214a747f1d32dcd74f022c11
Wael <wael@Waels-Mac-mini.local>
2024-12-14 22:35:47 +0200

    enhance app

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,20 +3,20 @@ import Sections from "../_components/Sections";
 import Shopping from "../_components/shopping/Shopping";
 import Popular from "../_components/popular/Popular";
 import Services from "../_components/services/Services";
-import { fetchData } from "../_util/fetchData";
+// import { fetchData } from "../_util/fetchData";
 
 
 export default async function page() {
 
-  const sections = await fetchData("api/sections");
-  const products = await fetchData("api/products");
+  // const sections = await fetchData("api/sections");
+  // const products = await fetchData("api/products");
 
   return (
     <>
     <Header />
-    <Sections storeMenu={sections} />
-    <Shopping sections={sections} />
-    <Popular products={products}  />
+    <Sections storeMenu={[]} />
+    <Shopping sections={[]} />
+    <Popular products={[]}  />
     <Services />
     </>
   );
