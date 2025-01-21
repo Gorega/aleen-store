8ce78adc4742893e05be4d45257eb9c4f1011fd2
Wael <waelabuawwad18@gmail.com>
2025-01-21 20:08:48 +0200

    change footer developer name

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -46,7 +46,7 @@ export default function Footer({storeMenu}){
             </div>
         </div>
         <div className={styles.outer}>
-            <span>تصميم وتطوير Gorega</span>
+            <span>تصميم وتطوير laialy</span>
         </div>
     </footer>
 }
